403f17040a33c601b7d02d0624cff78d13844f93
yuanzhengqiang <273816488@qq.com>
2017-03-30 08:50:48 +0800

    Initial Commit

---
+++ textFieldDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  textFieldDemo
+//
+//  Created by 原政强 on 17/3/30.
+//  Copyright © 2017年 yzq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
